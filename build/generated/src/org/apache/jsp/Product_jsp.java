968375c30644ccf6640c365cf1921f2d07b825c1
Tuan143 <Tuantvhe151027@fpt.edu.vn>
2022-02-28 22:02:53 +0700

    Product

---
+++ build/generated/src/org/apache/jsp/Product_jsp.java
@@ -0,0 +1,82 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Product_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css\">\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js\"></script>\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js\"></script>\n");
+      out.write("        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js\"></script>\n");
+      out.write("        <title>Product</title>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "Header", out, false);
+      out.write("\n");
+      out.write("            <div class=\"row\">\n");
+      out.write("                <div class=\"col-md-3\">left</div>\n");
+      out.write("                <div class=\"col-md-9\">right</div>\n");
+      out.write("            </div>\n");
+      out.write("        ");
+      org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "Footer.jsp", out, false);
+      out.write("\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
--- build/web/Product.jsp
+++ build/web/Product.jsp
@@ -1,6 +1,6 @@
 <%-- 
     Document   : Product
-    Created on : Feb 26, 2022, 10:55:56 PM
+    Created on : Feb 28, 2022, 9:06:15 PM
     Author     : Tún ^^
 --%>
 
@@ -13,12 +13,42 @@
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="css/product.css">
         <title>Product</title>
     </head>
     <body>
-        <jsp:include page="Header"></jsp:include>
-            <div>
-
+        <jsp:include page="Header.jsp"></jsp:include>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 left-slide">
+                        <h4>Phân loại sản phẩm</h4>
+                        <hr>
+                        <ul class="category-style">
+                            <li>
+                                <a href="#">Apple</a>
+                            </li>
+                            <li>
+                                <a href="#">Samsung</a>
+                            </li>
+                            <li>
+                                <a href="#">Xiaomi</a>
+                            </li>
+                            <li>
+                                <a href="#">Oppo</a>
+                            </li>
+                            <li>
+                                <a href="#">Realme</a>
+                            </li>
+                            <li>
+                                <a href="#">Nokia</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-9">
+                        <h4>Danh sách sản phẩm</h4>
+                        <hr>
+                    </div>
+                </div>
             </div>
         <jsp:include page="Footer.jsp"></jsp:include>
     </body>
